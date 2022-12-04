7a6e125cc430bd1655fe07162734ba9f6e8d197e
Carlos Oliveira <carlos.oliveira@coverflex.com>
2022-12-04 15:31:06 +0000

    day 3 part 2

--- apps/day3/lib/day3.ex
+++ apps/day3/lib/day3.ex
@@ -61,6 +61,13 @@ defmodule Day3 do
   @spec solve(binary() | list(), keyword()) :: number()
   def solve(file_path_or_list, opts \\ [])
 
+  def solve(rucksacks, part: 2) when is_list(rucksacks) do
+    rucksacks
+    |> Enum.chunk_every(3)
+    |> Enum.map(&get_badge_priority/1)
+    |> Enum.sum()
+  end
+
   def solve(rucksacks, _opts) when is_list(rucksacks) do
     rucksacks
     |> Enum.map(&get_priority/1)
@@ -83,7 +90,7 @@ defmodule Day3 do
     rucksack
     |> split_in(2)
     |> get_shared_item()
-    |> then(&(Enum.find_index(@letters, fn letter -> &1 == letter end) + 1))
+    |> calculate_item_priority()
   end
 
   defp split_in(rucksack, num_compartments) do
@@ -106,5 +113,23 @@ defmodule Day3 do
     compartment
     |> String.split("", trim: true)
     |> MapSet.new()
+  end
+
+  defp calculate_item_priority(item) do
+    Enum.find_index(@letters, &(&1 == item)) + 1
+  end
+
+  defp get_badge_priority(rucksack_group) do
+    rucksack_group
+    |> Enum.map(&build_set/1)
+    |> find_badge()
+    |> calculate_item_priority()
+  end
+
+  defp find_badge([first_set | rest_of_the_group]) do
+    rest_of_the_group
+    |> Enum.reduce(first_set, &MapSet.intersection(&1, &2))
+    |> MapSet.to_list()
+    |> List.first()
   end
 end
--- apps/day3/test/day3_test.exs
+++ apps/day3/test/day3_test.exs
@@ -21,4 +21,25 @@ defmodule Day3Test do
       assert 8018 == Day3.solve(file_path)
     end
   end
+
+  describe "solve part 2" do
+    test "base case" do
+      input = [
+        "vJrwpWtwJgWrhcsFMMfFFhFp",
+        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+        "PmmdzqPrVvPwwTWBwg",
+        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+        "ttgJtRGJQctTZtZT",
+        "CrZsJsPPZsGzwwsLwLmpwMDw"
+      ]
+
+      assert 70 == Day3.solve(input, part: 2)
+    end
+
+    test "challenge" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 2518 == Day3.solve(file_path, part: 2)
+    end
+  end
 end
