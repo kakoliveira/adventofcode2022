cd09de30ccda44a4008add7f8a60a505e981b3f2
Carlos Oliveira <carlos.oliveira@coverflex.com>
2022-12-06 18:37:10 +0000

    day 6 part 2

--- apps/day6/lib/day6.ex
+++ apps/day6/lib/day6.ex
@@ -6,12 +6,12 @@ defmodule Day6 do
   @spec solve(binary() | list(), keyword()) :: number()
   def solve(file_path_or_list, opts \\ [])
 
-  def solve([signal | _], _opts) do
+  def solve([signal | _], maker_size: maker_size) when is_integer(maker_size) do
     signal
     |> String.split("", trim: true)
-    |> Enum.chunk_every(4, 1)
-    |> Enum.find_index(&marker?/1)
-    |> Kernel.+(4)
+    |> Enum.chunk_every(maker_size, 1)
+    |> Enum.find_index(&marker?(&1, maker_size))
+    |> Kernel.+(maker_size)
   end
 
   def solve(file_path, opts) do
@@ -24,10 +24,10 @@ defmodule Day6 do
     FileReader.read(file_path)
   end
 
-  defp marker?(chunk) do
+  defp marker?(chunk, maker_size) do
     chunk
     |> MapSet.new()
     |> MapSet.size()
-    |> Kernel.==(4)
+    |> Kernel.==(maker_size)
   end
 end
--- apps/day6/test/day6_test.exs
+++ apps/day6/test/day6_test.exs
@@ -5,13 +5,27 @@ defmodule Day6Test do
     test "base case" do
       input = ["mjqjpqmgbljsphdztnvjfqwrcgsmlb"]
 
-      assert 7 == Day6.solve(input)
+      assert 7 == Day6.solve(input, maker_size: 4)
     end
 
     test "challenge" do
       file_path = Path.join(File.cwd!(), "input.txt")
 
-      assert 1802 == Day6.solve(file_path)
+      assert 1802 == Day6.solve(file_path, maker_size: 4)
+    end
+  end
+
+  describe "solve part 2" do
+    test "base case" do
+      input = ["mjqjpqmgbljsphdztnvjfqwrcgsmlb"]
+
+      assert 19 == Day6.solve(input, maker_size: 14)
+    end
+
+    test "challenge" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 1802 == Day6.solve(file_path, maker_size: 14)
     end
   end
 end
