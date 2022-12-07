a35a655141a18bd1ed7992f73b334581d4fb4c83
Carlos Oliveira <carlos.oliveira@coverflex.com>
2022-12-07 22:11:33 +0000

    day 7 refactor part 2

--- apps/day7/lib/day7.ex
+++ apps/day7/lib/day7.ex
@@ -6,23 +6,23 @@ defmodule Day7 do
   @spec solve(binary() | list(), keyword()) :: number()
   def solve(file_path_or_list, opts \\ [])
 
-  def solve(["$ cd /" | terminal_output], max_dir_size: max_dir_size)
+  def solve(terminal_output, max_dir_size: max_dir_size)
       when is_list(terminal_output) and is_integer(max_dir_size) do
     terminal_output
-    |> process_terminal_output(%{name: "/", children: [], size: 0})
-    |> extract_directories_by_size(max_dir_size)
+    |> process_terminal_output()
+    |> extract_directories_by_size(max: max_dir_size)
     |> Enum.map(& &1.size)
     |> Enum.sum()
   end
 
-  def solve(["$ cd /" | terminal_output],
+  def solve(terminal_output,
         disk_space: disk_space,
         min_needed_free_space: min_needed_free_space
       )
       when is_list(terminal_output) and is_integer(disk_space) and
              is_integer(min_needed_free_space) do
     terminal_output
-    |> process_terminal_output(%{name: "/", children: [], size: 0})
+    |> process_terminal_output()
     |> find_suitable_directories_to_delete(disk_space, min_needed_free_space)
     |> Enum.sort_by(& &1.size)
     |> List.first()
@@ -40,6 +40,9 @@ defmodule Day7 do
     |> FileReader.read()
     |> FileReader.clean_content()
   end
+
+  defp process_terminal_output(["$ cd /" | terminal_output]),
+    do: process_terminal_output(terminal_output, %{name: "/", children: [], size: 0})
 
   defp process_terminal_output([], directory), do: directory
 
@@ -71,7 +74,7 @@ defmodule Day7 do
   defp update_directory({:cd, directory_name}, directory, terminal_output) do
     terminal_output
     |> process_subdirectory(%{name: directory_name, children: [], size: 0})
-    |> update_children(directory)
+    |> update_with_subdirectory(directory)
   end
 
   defp update_directory({:dir, directory_name}, directory, terminal_output) do
@@ -106,42 +109,48 @@ defmodule Day7 do
     |> then(&process_subdirectory(elem(&1, 1), elem(&1, 0)))
   end
 
-  defp update_children({%{name: dir_name, size: size} = child, terminal_output}, directory) do
+  defp update_with_subdirectory({%{size: size} = child, terminal_output}, directory) do
     directory
-    |> Map.update!(:children, fn children ->
-      children
-      |> Enum.find_index(&(Map.get(&1, :name) == dir_name))
-      |> then(&List.replace_at(children, &1, child))
-    end)
+    |> Map.update!(:children, &set_subdirectory(&1, child))
     |> Map.update!(:size, &update_directory_size(&1, size))
     |> then(&{&1, terminal_output})
   end
 
-  defp extract_directories_by_size(%{children: children}, max_dir_size),
-    do: extract_directories_by_size(children, max_dir_size, [])
+  defp set_subdirectory(children, %{name: dir_name} = child) do
+    children
+    |> Enum.find_index(&(Map.get(&1, :name) == dir_name))
+    |> then(&List.replace_at(children, &1, child))
+  end
 
-  defp extract_directories_by_size([], _max_dir_size, directories), do: directories
+  defp extract_directories_by_size(%{children: children}, size_opts),
+    do: extract_directories_by_size(children, size_opts, [])
+
+  defp extract_directories_by_size([], _size_opts, directories), do: directories
 
   defp extract_directories_by_size(
          [%{name: _name} = directory | children],
-         max_dir_size,
+         size_opts,
          directories
        ) do
     directory
-    |> maybe_add_current_directory(max_dir_size)
-    |> Kernel.++(extract_directories_by_size(directory, max_dir_size))
+    |> maybe_add_current_directory(size_opts)
+    |> Kernel.++(extract_directories_by_size(directory, size_opts))
     |> Kernel.++(directories)
-    |> then(&extract_directories_by_size(children, max_dir_size, &1))
+    |> then(&extract_directories_by_size(children, size_opts, &1))
   end
 
-  defp extract_directories_by_size([_file | children], max_dir_size, directories),
-    do: extract_directories_by_size(children, max_dir_size, directories)
+  defp extract_directories_by_size([_file | children], size_opts, directories),
+    do: extract_directories_by_size(children, size_opts, directories)
 
-  defp maybe_add_current_directory(%{size: size} = directory, max_dir_size)
+  defp maybe_add_current_directory(%{size: size} = directory, max: max_dir_size)
        when size <= max_dir_size,
        do: [directory]
 
-  defp maybe_add_current_directory(_directory, _max_dir_size), do: []
+  defp maybe_add_current_directory(%{size: size} = directory, min: min_needed_space)
+       when size >= min_needed_space,
+       do: [directory]
+
+  defp maybe_add_current_directory(_directory, _size_opts), do: []
 
   defp find_suitable_directories_to_delete(
          %{children: children, size: root_dir_size},
@@ -151,29 +160,6 @@ defmodule Day7 do
     disk_space
     |> Kernel.-(root_dir_size)
     |> then(&Kernel.-(min_needed_free_space, &1))
-    |> then(&find_suitable_directories_to_delete(children, &1, []))
+    |> then(&extract_directories_by_size(children, [min: &1], []))
   end
-
-  defp find_suitable_directories_to_delete([], _min_needed_space, directories), do: directories
-
-  defp find_suitable_directories_to_delete(
-         [%{name: _name, children: subdirectory_children} = directory | children],
-         min_needed_space,
-         directories
-       ) do
-    directory
-    |> maybe_add_current_directory_2(min_needed_space)
-    |> Kernel.++(find_suitable_directories_to_delete(subdirectory_children, min_needed_space, []))
-    |> Kernel.++(directories)
-    |> then(&find_suitable_directories_to_delete(children, min_needed_space, &1))
-  end
-
-  defp find_suitable_directories_to_delete([_file | children], min_needed_space, directories),
-    do: find_suitable_directories_to_delete(children, min_needed_space, directories)
-
-  defp maybe_add_current_directory_2(%{size: size} = directory, min_needed_space)
-       when size >= min_needed_space,
-       do: [directory]
-
-  defp maybe_add_current_directory_2(_directory, _max_dir_size), do: []
 end
