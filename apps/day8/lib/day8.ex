99471967c0f976c23b5bb19afaaf0adf781f3d84
Carlos Oliveira <carlos.oliveira@coverflex.com>
2022-12-08 19:48:22 +0000

    day 8 refactor

--- apps/day8/lib/day8.ex
+++ apps/day8/lib/day8.ex
@@ -36,21 +36,59 @@ defmodule Day8 do
 
   defp find_visible_trees(point, acc, {tree_matrix, max_row_index, max_column_index}) do
     point
-    |> is_visible?(tree_matrix, max_row_index, max_column_index)
+    |> evaluate_point(tree_matrix, max_row_index, max_column_index, true, &is_visible?/2)
     |> update_acc(point, acc)
   end
 
-  defp is_visible?({0, _col_index}, _tree_matrix, _num_rows, _num_columns), do: true
-  defp is_visible?({_row_index, 0}, _tree_matrix, _num_rows, _num_columns), do: true
+  defp evaluate_point(
+         {0, _col_index},
+         _tree_matrix,
+         _max_row_index,
+         _max_column_index,
+         edge_value,
+         _evaluator
+       ),
+       do: edge_value
 
-  defp is_visible?({row_index, col_index}, _tree_matrix, num_rows, num_columns)
-       when row_index == num_rows or col_index == num_columns,
-       do: true
+  defp evaluate_point(
+         {_row_index, 0},
+         _tree_matrix,
+         _max_row_index,
+         _max_column_index,
+         edge_value,
+         _evaluator
+       ),
+       do: edge_value
 
-  defp is_visible?({row_index, col_index}, tree_matrix, _num_rows, _num_columns) do
+  defp evaluate_point(
+         {row_index, col_index},
+         _tree_matrix,
+         max_row_index,
+         max_column_index,
+         edge_value,
+         _evaluator
+       )
+       when row_index == max_row_index or col_index == max_column_index,
+       do: edge_value
+
+  defp evaluate_point(
+         {row_index, col_index},
+         tree_matrix,
+         _max_row_index,
+         _max_column_index,
+         _edge_value,
+         evaluator
+       )
+       when is_function(evaluator, 2) do
     tree_matrix
     |> Util.Matrix.get_point(row_index, col_index)
-    |> is_visible?(tree_matrix)
+    |> evaluator.(tree_matrix)
+  end
+
+  defp is_visible?({tree_to_the_left, tree_to_the_right}, value) do
+    with false <- all_smaller?(tree_to_the_left, value) do
+      all_smaller?(tree_to_the_right, value)
+    end
   end
 
   defp is_visible?(point, tree_matrix) do
@@ -59,52 +97,53 @@ defmodule Day8 do
     end
   end
 
-  defp is_visible_by_column?({value, row_index, column_index}, tree_matrix) do
-    tree_matrix
-    |> Util.Matrix.fetch_column(column_index)
-    |> Enum.with_index()
-    |> Enum.split_while(&(elem(&1, 1) != row_index))
-    |> is_visible_by_row?(value)
+  defp all_smaller?(trees, value) do
+    Enum.all?(trees, &(elem(&1, 0) < value))
   end
 
-  defp is_visible_by_row?({value, row_index, column_index}, tree_matrix) do
+  defp is_visible_by_row?(point, tree_matrix),
+    do: evaluate_row(point, tree_matrix, &is_visible?/2)
+
+  defp is_visible_by_column?(point, tree_matrix),
+    do: evaluate_column(point, tree_matrix, &is_visible?/2)
+
+  defp evaluate_row({value, row_index, column_index}, tree_matrix, evaluator)
+       when is_function(evaluator, 2) do
     tree_matrix
     |> Enum.at(row_index)
     |> Enum.with_index()
     |> Enum.split_while(&(elem(&1, 1) != column_index))
-    |> is_visible_by_row?(value)
+    |> remove_current_point()
+    |> evaluator.(value)
   end
 
-  defp is_visible_by_row?({tree_to_the_left, [_current_tree | tree_to_the_right]}, value) do
-    with false <- all_smaller?(tree_to_the_left, value) do
-      all_smaller?(tree_to_the_right, value)
-    end
+  defp evaluate_column({value, row_index, column_index}, tree_matrix, evaluator)
+       when is_function(evaluator, 2) do
+    tree_matrix
+    |> Util.Matrix.fetch_column(column_index)
+    |> Enum.with_index()
+    |> Enum.split_while(&(elem(&1, 1) != row_index))
+    |> remove_current_point()
+    |> evaluator.(value)
   end
 
-  defp all_smaller?(trees, value) do
-    Enum.all?(trees, &(elem(&1, 0) < value))
-  end
+  defp remove_current_point({tree_to_the_left, [_current_tree | tree_to_the_right]}),
+    do: {tree_to_the_left, tree_to_the_right}
 
   defp update_acc(true, point, acc), do: [point] ++ acc
   defp update_acc(_false, _point, acc), do: acc
 
   defp find_best_scenic_view_tree(point, acc, {tree_matrix, max_row_index, max_column_index}) do
     point
-    |> calculate_scenic_score(tree_matrix, max_row_index, max_column_index)
+    |> evaluate_point(tree_matrix, max_row_index, max_column_index, 0, &calculate_scenic_score/2)
     |> keep_best_point(acc)
   end
 
-  defp calculate_scenic_score({0, _col_index}, _tree_matrix, _num_rows, _num_columns), do: 0
-  defp calculate_scenic_score({_row_index, 0}, _tree_matrix, _num_rows, _num_columns), do: 0
-
-  defp calculate_scenic_score({row_index, col_index}, _tree_matrix, num_rows, num_columns)
-       when row_index == num_rows or col_index == num_columns,
-       do: 0
-
-  defp calculate_scenic_score({row_index, col_index}, tree_matrix, _num_rows, _num_columns) do
-    tree_matrix
-    |> Util.Matrix.get_point(row_index, col_index)
-    |> calculate_scenic_score(tree_matrix)
+  defp calculate_scenic_score({tree_to_the_left, tree_to_the_right}, value) do
+    tree_to_the_left
+    |> Enum.reverse()
+    |> calculate_viewing_distance(value)
+    |> Kernel.*(calculate_viewing_distance(tree_to_the_right, value))
   end
 
   defp calculate_scenic_score(point, tree_matrix) do
@@ -112,41 +151,20 @@ defmodule Day8 do
       calculate_scenic_score_column(point, tree_matrix)
   end
 
-  defp calculate_scenic_score_column({value, row_index, column_index}, tree_matrix) do
-    tree_matrix
-    |> Util.Matrix.fetch_column(column_index)
-    |> Enum.with_index()
-    |> Enum.split_while(&(elem(&1, 1) != row_index))
-    |> calculate_scenic_score_row(value)
-  end
+  defp calculate_scenic_score_row(point, tree_matrix),
+    do: evaluate_row(point, tree_matrix, &calculate_scenic_score/2)
 
-  defp calculate_scenic_score_row({value, row_index, column_index}, tree_matrix) do
-    tree_matrix
-    |> Enum.at(row_index)
-    |> Enum.with_index()
-    |> Enum.split_while(&(elem(&1, 1) != column_index))
-    |> calculate_scenic_score_row(value)
-  end
-
-  defp calculate_scenic_score_row({tree_to_the_left, [_current_tree | tree_to_the_right]}, value) do
-    tree_to_the_left
-    |> Enum.reverse()
-    |> calculate_viewing_distance(value)
-    |> Kernel.*(calculate_viewing_distance(tree_to_the_right, value))
-  end
+  defp calculate_scenic_score_column(point, tree_matrix),
+    do: evaluate_column(point, tree_matrix, &calculate_scenic_score/2)
 
   defp calculate_viewing_distance([_one_tree], _value), do: 1
 
   defp calculate_viewing_distance(trees, value) do
-    trees
-    |> Enum.reduce_while(0, fn {tree, _index}, acc ->
-      if value > tree do
-        {:cont, acc + 1}
-      else
-        {:halt, acc + 1}
-      end
-    end)
+    Enum.reduce_while(trees, 0, &count_visible_tree(&1, &2, value))
   end
+
+  defp count_visible_tree({tree, _index}, acc, value) when value > tree, do: {:cont, acc + 1}
+  defp count_visible_tree(_tree, acc, _value), do: {:halt, acc + 1}
 
   defp keep_best_point(new_score, nil), do: new_score
   defp keep_best_point(new_score, current_score) when new_score > current_score, do: new_score
