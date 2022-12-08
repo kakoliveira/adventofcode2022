2789f42ae76adfe2347231da0e7a3443ed029fe7
Carlos Oliveira <carlos.oliveira@coverflex.com>
2022-12-08 18:53:53 +0000

    day 8 part 2

--- apps/day8/lib/day8.ex
+++ apps/day8/lib/day8.ex
@@ -6,11 +6,21 @@ defmodule Day8 do
   @spec solve(binary() | list(), keyword()) :: number()
   def solve(file_path_or_list, opts \\ [])
 
-  def solve(tree_heights, _opts) when is_list(tree_heights) do
+  def solve(tree_heights, opts) when is_list(tree_heights) do
     tree_heights
     |> Util.Matrix.parse_matrix(to_integer: true)
     |> Util.Matrix.describe()
     |> convert_to_index()
+    |> solve(opts)
+  end
+
+  def solve(tree_heights, scenic_score: true) when is_tuple(tree_heights) do
+    tree_heights
+    |> find_best_scenic_view_tree()
+  end
+
+  def solve(tree_heights, _opts) when is_tuple(tree_heights) do
+    tree_heights
     |> find_visible_trees()
     |> length()
   end
@@ -91,4 +101,76 @@ defmodule Day8 do
 
   defp update_acc(true, point, acc), do: [point] ++ acc
   defp update_acc(_false, _point, acc), do: acc
+
+  defp find_best_scenic_view_tree({tree_matrix, max_row_index, max_column_index}) do
+    0..max_row_index
+    |> Enum.reduce(fn row_index, acc ->
+      0..max_column_index
+      |> Enum.reduce(acc, fn col_index, acc ->
+        point = {row_index, col_index}
+
+        point
+        |> calculate_scenic_score(tree_matrix, max_row_index, max_column_index)
+        |> keep_best_point(acc)
+      end)
+    end)
+  end
+
+  defp calculate_scenic_score({0, _col_index}, _tree_matrix, _num_rows, _num_columns), do: 0
+  defp calculate_scenic_score({_row_index, 0}, _tree_matrix, _num_rows, _num_columns), do: 0
+
+  defp calculate_scenic_score({row_index, col_index}, _tree_matrix, num_rows, num_columns)
+       when row_index == num_rows or col_index == num_columns,
+       do: 0
+
+  defp calculate_scenic_score({row_index, col_index}, tree_matrix, _num_rows, _num_columns) do
+    tree_matrix
+    |> Util.Matrix.get_point(row_index, col_index)
+    |> calculate_scenic_score(tree_matrix)
+  end
+
+  defp calculate_scenic_score(point, tree_matrix) do
+    calculate_scenic_score_row(point, tree_matrix) *
+      calculate_scenic_score_column(point, tree_matrix)
+  end
+
+  defp calculate_scenic_score_column({value, row_index, column_index}, tree_matrix) do
+    tree_matrix
+    |> Util.Matrix.fetch_column(column_index)
+    |> Enum.with_index()
+    |> Enum.split_while(&(elem(&1, 1) != row_index))
+    |> calculate_scenic_score_row(value)
+  end
+
+  defp calculate_scenic_score_row({value, row_index, column_index}, tree_matrix) do
+    tree_matrix
+    |> Enum.at(row_index)
+    |> Enum.with_index()
+    |> Enum.split_while(&(elem(&1, 1) != column_index))
+    |> calculate_scenic_score_row(value)
+  end
+
+  defp calculate_scenic_score_row({tree_to_the_left, [_current_tree | tree_to_the_right]}, value) do
+    tree_to_the_left
+    |> Enum.reverse()
+    |> calculate_viewing_distance(value)
+    |> Kernel.*(calculate_viewing_distance(tree_to_the_right, value))
+  end
+
+  defp calculate_viewing_distance([_one_tree], _value), do: 1
+
+  defp calculate_viewing_distance(trees, value) do
+    trees
+    |> Enum.reduce_while(0, fn {tree, _index}, acc ->
+      if value > tree do
+        {:cont, acc + 1}
+      else
+        {:halt, acc + 1}
+      end
+    end)
+  end
+
+  defp keep_best_point(new_score, nil), do: new_score
+  defp keep_best_point(new_score, current_score) when new_score > current_score, do: new_score
+  defp keep_best_point(_new_score, current_score), do: current_score
 end
--- apps/day8/test/day8_test.exs
+++ apps/day8/test/day8_test.exs
@@ -20,4 +20,24 @@ defmodule Day8Test do
       assert 1859 == Day8.solve(file_path)
     end
   end
+
+  describe "solve part 2" do
+    test "base case" do
+      input = [
+        "30373",
+        "25512",
+        "65332",
+        "33549",
+        "35390"
+      ]
+
+      assert 8 == Day8.solve(input, scenic_score: true)
+    end
+
+    test "challenge" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 1859 == Day8.solve(file_path, scenic_score: true)
+    end
+  end
 end
