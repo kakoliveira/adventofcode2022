f75e7c3b5c1b46e6568ec641de2aa5cc4f4fdc2c
Carlos Oliveira <carlos.oliveira@coverflex.com>
2022-12-09 20:20:36 +0000

    day 9 part 2 (given up)

--- apps/day9/test/day9_test.exs
+++ apps/day9/test/day9_test.exs
@@ -23,4 +23,27 @@ defmodule Day9Test do
       assert 6498 == Day9.solve(file_path)
     end
   end
+
+  describe "solve part 2" do
+    test "base case" do
+      input = [
+        "R 5",
+        "U 8",
+        "L 8",
+        "D 3",
+        "R 17",
+        "D 10",
+        "L 25",
+        "U 20"
+      ]
+
+      assert 36 == Day9.solve(input, knots: 10)
+    end
+
+    test "challenge" do
+      file_path = Path.join(File.cwd!(), "input.txt")
+
+      assert 6498 == Day9.solve(file_path, knots: 10)
+    end
+  end
 end
